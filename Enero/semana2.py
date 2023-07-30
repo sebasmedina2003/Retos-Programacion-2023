af67f11d1bec00f83f47c4b33b0b7e4815fb1015
Sebastian Medina <sebasmedina2003@gmail.com>
2023-07-30 00:09:25 -0400

    Lenguaje Hacker

---
+++ Enero/semana2.py
@@ -0,0 +1,38 @@
+def LenguajeHacker(palabra: str) -> str:
+    diccionario = {
+        'a': '4',
+        'b': 'I3',
+        'c': '[',
+        'd': ')',
+        'e': '3',
+        'f': '|=',
+        'g': '&',
+        'h': '#',
+        'i': '1',
+        'j': ',_|',
+        'k': '>|',
+        'l': '1',
+        'm': '/\/\\',
+        'n': '^/',
+        'o': '0',
+        'p': '|*',
+        'q': '(_,)',
+        'r': 'I2',
+        's': '5',
+        't': '7',
+        'u': '(_)',
+        'v': '\/',
+        'w': '\/\/',
+        'x': '><',
+        'y': 'j',
+        'z': '2'
+    }
+    resultado = ''
+
+    for letra in palabra:
+        resultado += diccionario[letra] if letra.lower in diccionario else letra
+
+    return resultado
+
+
+print(LenguajeHacker('leet hola'))
